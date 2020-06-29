df7e9519fd4e2957236793b25b7b1957524e1712
Saizzou <65252447+Saizzou@users.noreply.github.com>
2020-06-29 20:49:59 +0200

    Update uitopy.bat

--- uitopy.bat
+++ uitopy.bat
@@ -3,11 +3,11 @@
 
 cd "directory of pyuic5.exe"
 
-set /p changer=Degistirmek istediginiz ui dosyasi nedir?
+set /p changer=Which .ui file do you want to change?
 
 pyuic5 -x %changer%.ui -o %girdi%.py
 
 
 cls
-Change is done.
+echo Change is done.
 goto start
